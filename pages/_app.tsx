06070a8ee14000c69bed31a9a30d948be6f39cc8
Frankie Lam <frankie191193@gmail.com>
2022-09-30 14:45:08 +0100

    set up QueryClientProvider for page components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,10 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../src/components/layout";
 import Head from "next/head";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,9 +13,12 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </>
   );
 }
