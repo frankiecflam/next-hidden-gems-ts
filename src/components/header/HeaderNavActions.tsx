cfd7075e6a144c74769c7fe40d41cb137f8a11ae
Frankie Lam <frankie191193@gmail.com>
2022-09-30 11:12:38 +0100

    add firebase built-in methods to handle login, signup and signout to NavActions

--- src/components/header/HeaderNavActions.tsx
+++ src/components/header/HeaderNavActions.tsx
@@ -1,15 +1,52 @@
 import styles from "./HeaderNavActions.module.css";
+import { useRouter } from "next/router";
+import {
+  signInWithGooglePopup,
+  signOutWithGoogle,
+} from "../../config/firebase";
 
 const HeaderNavActions = () => {
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    try {
+      const response = await signInWithGooglePopup();
+
+      if (!response.user)
+        throw new Error("Failed to sign in with Google account!");
+
+      router.push("/");
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await signOutWithGoogle();
+    } catch (error: any) {
+      console.log("Failed to sign out!");
+    }
+  };
+
   return (
     <div className={styles.navActions}>
-      <button className={`${styles.navActionsBtn} ${styles.login}`}>
+      <button
+        className={`${styles.navActionsBtn} ${styles.login}`}
+        onClick={handleLogin}
+      >
         log in
       </button>
-      <button className={`${styles.navActionsBtn} ${styles.signup}`}>
+      <button
+        className={`${styles.navActionsBtn} ${styles.signup}`}
+        onClick={handleLogin}
+      >
         sign up
       </button>
-      <button className={`${styles.navActionsBtn} ${styles.logout}`}>
+      <button
+        className={`${styles.navActionsBtn} ${styles.logout}`}
+        onClick={handleLogout}
+      >
         log out
       </button>
     </div>
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,13 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: "AIzaSyDOGTaZgByOFjzyRViNrvP5JDc2_5edPDk",
   authDomain: "next-hidden-gems.firebaseapp.com",
   projectId: "next-hidden-gems",
   storageBucket: "next-hidden-gems.appspot.com",
@@ -21,3 +26,7 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const authProvider = new GoogleAuthProvider();
+
+// Sign-in methods
+export const signInWithGooglePopup = () => signInWithPopup(auth, authProvider);
+export const signOutWithGoogle = () => signOut(auth);
