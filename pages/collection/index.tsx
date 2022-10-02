24167510c83dda5925a1bb1413fe3c6603d1cabc
Frankie Lam <frankie191193@gmail.com>
2022-10-02 09:26:29 +0100

    install next-firebase-auth to do authentication in SSR for proetected pages

--- pages/collection/index.tsx
+++ pages/collection/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from "next";
+import { GetServerSideProps } from "next";
+import { useFirebaseAuthState } from "../../src/hooks";
 
 const Collection: NextPage = () => {
   return <div>Collection Page</div>;
 };
 
 export default Collection;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    props: {},
+  };
+};
