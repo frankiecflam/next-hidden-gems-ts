7db8707ca0ed84b41648624d31f9f40f8c356ea1
Frankie Lam <frankie191193@gmail.com>
2022-10-04 15:39:06 +0100

    replace firebaseConfig's details with env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -10,13 +10,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDOGTaZgByOFjzyRViNrvP5JDc2_5edPDk",
-  authDomain: "next-hidden-gems.firebaseapp.com",
-  projectId: "next-hidden-gems",
-  storageBucket: "next-hidden-gems.appspot.com",
-  messagingSenderId: "1012435499783",
-  appId: "1:1012435499783:web:b116eff5560a1033d5a3a2",
-  measurementId: "G-R79KSFDQ7K",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
