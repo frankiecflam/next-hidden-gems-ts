6d31525b59b42d9c6ae1e1464f92d93bbfb11b0f
Frankie Lam <frankie191193@gmail.com>
2022-10-06 11:00:25 +0100

    add signin and signup pages

---
+++ pages/account/signin/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const SignInPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Hidden Gems — Sign In</title>
+        <meta
+          name="description"
+          content="Sign in now to discover hidden gems from people across the world!"
+        />
+      </Head>
+    </>
+  );
+};
+
+export default SignInPage;
---
+++ pages/account/signup/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const SignUpPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Hidden Gems — Sign Up</title>
+        <meta
+          name="description"
+          content="Join now to discover hidden gems from people across the world!"
+        />
+      </Head>
+    </>
+  );
+};
+
+export default SignUpPage;
