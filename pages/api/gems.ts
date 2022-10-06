9c88de742baae1d34739ec27bb668d9ddb1b9c8f
Frankie Lam <frankie191193@gmail.com>
2022-10-06 10:10:34 +0100

    configure the gems api endpoint to handle POST request

--- pages/api/gems.ts
+++ pages/api/gems.ts
@@ -1,7 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { collection, getDocs, DocumentData } from "firebase/firestore";
+import { collection, getDocs, DocumentData, addDoc } from "firebase/firestore";
 import { db } from "../../src/config/firebase";
+import Gem from "../../src/types/gem";
 
 type Data = {
   gems?: DocumentData[];
@@ -12,25 +13,52 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  if (req.method !== "GET")
-    return res.status(400).json({ error: "Only GET method is allowed!" });
+  if (req.method === "GET") {
+    try {
+      const gemRef = collection(db, "gems");
 
-  try {
-    const gemRef = collection(db, "gems");
+      const querySnapshot = await getDocs(gemRef);
 
-    const querySnapshot = await getDocs(gemRef);
+      if (querySnapshot.empty)
+        throw new Error("No matching documents in gems collection");
 
-    if (querySnapshot.empty)
-      throw new Error("No matching documents in gems collection");
+      let docs: DocumentData[] = [];
 
-    let docs: DocumentData[] = [];
+      querySnapshot.forEach((doc) => {
+        docs.push(doc.data());
+      });
 
-    querySnapshot.forEach((doc) => {
-      docs.push(doc.data());
-    });
-
-    res.status(200).json({ gems: docs });
-  } catch (error: any) {
-    res.status(400).json({ error });
+      return res.status(200).json({ gems: docs });
+    } catch (error: any) {
+      return res.status(400).json({ error });
+    }
   }
+
+  if (req.method === "POST") {
+    const { newGem }: { newGem: Gem } = req.body;
+
+    try {
+      const gemsRef = collection(db, "gems");
+      await addDoc(gemsRef, newGem);
+
+      const querySnapshot = await getDocs(gemsRef);
+
+      if (querySnapshot.empty)
+        throw new Error("No matching documents in gems collection");
+
+      let docs: DocumentData[] = [];
+
+      querySnapshot.forEach((doc) => {
+        docs.push(doc.data());
+      });
+
+      return res.status(200).json({ gems: docs });
+    } catch (error: any) {
+      return res.status(400).json({ error });
+    }
+  }
+
+  return res
+    .status(400)
+    .json({ error: "Only GET and POST method are allowed!" });
 }
