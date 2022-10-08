0dc8d6b1d15f24b7090e0d98beb5bb9b47e06e0e
Frankie Lam <frankie191193@gmail.com>
2022-10-08 11:16:06 +0100

    modifiy the function to use Generics

--- src/utils/helpers/checkIsUserCredentialType.ts
+++ src/utils/helpers/checkIsUserCredentialType.ts
@@ -1,8 +1,19 @@
 import { UserCredential } from "firebase/auth";
 
 // Check if ReturnType of signInWithGooglePopUp is UserCredential by Type Assertion
+
+/* Approach 1
 export default function checkIsUserCredentialType(
   result: UserCredential | void
 ): result is UserCredential {
+  return (result as UserCredential).user !== undefined;
+}
+
+*/
+
+/* Approach 2: Generics */
+export default function checkIsUserCredentialType<
+  T extends void | UserCredential
+>(result: T): boolean {
   return (result as UserCredential).user !== undefined;
 }
