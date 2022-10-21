de991e25c8d37cebb92b86db47760f601225c148
Frankie Lam <frankie191193@gmail.com>
2022-10-21 16:48:56 +0100

    refactor the feature of signinalternatives into a separate component

--- src/components/account/signin/SignInForm.tsx
+++ src/components/account/signin/SignInForm.tsx
@@ -1,6 +1,5 @@
 import styles from "./SignInForm.module.css";
-import Link from "next/link";
-import { SignInWithGooglePopUp } from "./";
+import { SignInWithGooglePopUp, SignInAlternatives } from "./";
 
 const SignInForm = () => {
   return (
@@ -10,19 +9,7 @@ const SignInForm = () => {
       </header>
       <div className={styles.body}>
         <SignInWithGooglePopUp />
-
-        <div className={styles.alternatives}>
-          <div className={styles.signupAlternative}>
-            <p>
-              Or
-              <br />
-              If you do not have an account,
-            </p>
-            <Link href="/account/signup">
-              <a className={styles.signUpLink}>sign up</a>
-            </Link>
-          </div>
-        </div>
+        <SignInAlternatives />
       </div>
     </section>
   );
--- src/components/account/signin/index.ts
+++ src/components/account/signin/index.ts
@@ -1,2 +1,3 @@
 export { default as SignInForm } from "./SignInForm";
 export { default as SignInWithGooglePopUp } from "./SignInWithGooglePopUp";
+export { default as SignInAlternatives } from "./SignInAlternatives";
