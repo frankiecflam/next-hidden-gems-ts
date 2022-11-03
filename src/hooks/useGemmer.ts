eb2d1035507a79a7147b2636a4f2f5e3c5db368a
Frankie Lam <frankie191193@gmail.com>
2022-11-03 16:28:16 +0000

    add gemmers schema to validate the api response

--- src/hooks/useGemmer.ts
+++ src/hooks/useGemmer.ts
@@ -1,13 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Gemmer from "../types/gemmer";
+import { gemmers as gemmersSchema } from "../schema/gemmers.schema";
 
 const useGemmer = (gemmerId: string) => {
   return useQuery(
     ["gemmers", gemmerId],
-    () => axios.get("/api/gemmers").then((res) => res.data.gemmers as Gemmer[]),
+    () =>
+      axios.get("/api/gemmers").then((res) => {
+        const data = res.data.gemmers;
+
+        return gemmersSchema.parse(data) as Gemmer[];
+      }),
     {
       select: (gemmers) => gemmers.find((gemmer) => gemmer.id === gemmerId),
+      onError: (error) => {
+        console.log(
+          "Error occured during zod parsing the api response of Gemmers."
+        );
+        console.error(error);
+      },
     }
   );
 };
