60e0b001e2f4002fa949eb48de8d694de9556882
Frankie Lam <frankie191193@gmail.com>
2022-11-03 14:53:13 +0000

    move the gem schema into an individual schema variable and name-export the schemas

--- src/schema/gems.schema.ts
+++ src/schema/gems.schema.ts
@@ -1,18 +1,16 @@
 import { z } from "zod";
 
-const gemsSchema = z.array(
-  z.object({
-    id: z.string(),
-    title: z.string(),
-    description: z.string(),
-    image: z.string(),
-    date: z.object({
-      seconds: z.number(),
-      nanoseconds: z.number(),
-    }),
-    categoryId: z.string(),
-    gemmerId: z.string(),
-  })
-);
+export const gem = z.object({
+  id: z.string(),
+  title: z.string(),
+  description: z.string(),
+  image: z.string(),
+  date: z.object({
+    seconds: z.number(),
+    nanoseconds: z.number(),
+  }),
+  categoryId: z.string(),
+  gemmerId: z.string(),
+});
 
-export default gemsSchema;
+export const gems = z.array(gem);
