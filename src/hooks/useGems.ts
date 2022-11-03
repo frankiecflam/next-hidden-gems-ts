686964faa06e4c439230f3ec41f1972400a84686
Frankie Lam <frankie191193@gmail.com>
2022-11-03 14:46:25 +0000

    add zod to validate api response from the gems api endpoint

--- src/hooks/useGems.ts
+++ src/hooks/useGems.ts
@@ -2,14 +2,41 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Gem from "../types/gem";
 import { sortGemsByDate } from "../utils/helpers";
+import { z } from "zod";
+
+const gemsSchema = z.array(
+  z.object({
+    id: z.string(),
+    title: z.string(),
+    description: z.string(),
+    image: z.string(),
+    date: z.object({
+      seconds: z.number(),
+      nanoseconds: z.number(),
+    }),
+    categoryId: z.string(),
+    gemmerId: z.string(),
+  })
+);
 
 const useGems = () => {
   return useQuery(
     ["gems"],
-    () => axios.get("/api/gems").then((res) => res.data.gems as Gem[]),
+    () =>
+      axios.get("/api/gems").then((res) => {
+        const data = res.data.gems;
+
+        return gemsSchema.parse(data) as Gem[];
+      }),
     {
       select: (gems: Gem[]) => {
         return sortGemsByDate(gems);
+      },
+      onError: (error) => {
+        console.log(
+          "Error occured during zod parsing the api response of Gems."
+        );
+        console.error(error);
       },
     }
   );
